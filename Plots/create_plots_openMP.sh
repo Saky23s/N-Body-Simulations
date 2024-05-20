27eeb0d5f7a98924fe383e0add70295c4102d384
Saky23s <curtur666@gmail.com>
2024-05-20 13:41:31 +0200

    Change in plots to easily compare OpenMP with cuda

---
+++ Plots/create_plots_openMP.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+# Check if exactly two parameters are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <Max_N> <Steps>"
+    exit 1
+fi
+
+#Delete old data just for safety
+if [ -d ../Graphics/data ]; then
+  rm -f ../Graphics/data/*.csv
+  rm -f ../Graphics/data/*.bin
+else
+  mkdir ../Graphics/data
+fi
+
+#Run time_simulation
+rm -f time_simulations_OpenMP
+make time_simulations_OpenMP >/dev/null
+./time_simulations_OpenMP $1 $2
+
+#plot results
+gnuplot> load 'times.p'
