67e8874987cdf3fc21ab079291661cfd0d6a38d3
Saky23s <curtur666@gmail.com>
2024-05-23 19:42:17 +0200

    Better run simulation script, combines all three scripts into one with optional flags

---
+++ Solucion_Ingenua/run_simulation.sh
@@ -0,0 +1,57 @@
+#!/bin/bash 
+
+# Check if exactly two parameters are provided
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <T> <Filepath> {-c -o -s -h}"
+    exit 1
+fi
+
+TYPE="secuencial";
+for arg in "$@" 
+do
+    case $arg in
+        -s) 
+            TYPE="secuencial"
+            ;;
+        -o) 
+            TYPE="OpenMP"
+            ;;
+        -c)
+            TYPE="cuda"
+            ;;
+        -h) # Explain arguments
+            echo "Usage: $0 <T> <Filepath> {-c -o -s}"
+            echo "-c: Run with cuda"
+            echo "-o: Run with OpenMP"
+            echo "-s (default): Run secuential"
+            echo "-h: Show help"
+            exit
+            ;;
+        *)
+            ;;
+    esac
+done
+
+#Delete old data
+if [ -d ../Graphics/data ]; then
+  rm -f ../Graphics/data/*.csv
+  rm -f ../Graphics/data/*.bin
+else
+  mkdir ../Graphics/data
+fi
+
+#Create starting configuration data
+cd ../Starting_Configurations
+make >/dev/null
+./graphic_starting_position $2
+
+#Run simulation
+cd ../Solucion_Ingenua/
+rm simulation_${TYPE} >/dev/null
+make simulation_${TYPE} >/dev/null
+./simulation_${TYPE} $1 $2
+
+#Run Graphics
+cd ../Graphics/
+cargo run
+cd ../Solucion_Ingenua/
