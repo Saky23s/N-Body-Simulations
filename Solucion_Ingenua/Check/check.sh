cc9b8ed0fdb707809c1ab6593a06cc75af28ac77
Saky23s <curtur666@gmail.com>
2024-05-25 00:25:30 +0200

    Checking script now checks OpenMP implementation

--- Solucion_Ingenua/Check/check.sh
+++ Solucion_Ingenua/Check/check.sh
@@ -6,10 +6,10 @@ YELLOW='\033[43m'
 NC='\033[m'
 CLEAR_LINE='\033[K'
 
-# Check if exactly two parameters are provided
-if [ "$#" -ne 3 ]; then
+# Check al least the tree must have parameters are provided
+if [ "$#" -lt 3 ]; then
     echo "Usage: $0 <start> <end> <step>"
-    exit 1
+    exit 0
 fi
 
 
@@ -23,6 +23,32 @@ make clean *>/dev/null
 make all  >/dev/null
 
 cd Check/
+
+cuda=false
+Open_MP=false
+
+for arg in "$@" 
+do
+    case $arg in
+        -co)
+            cuda=true
+            Open_MP=true
+            ;;
+        -oc)
+            cuda=true
+            Open_MP=true
+            ;;
+        -o) 
+            
+            Open_MP=true
+            ;;
+        -c) 
+            cuda=true
+            ;;
+        *)
+            ;;
+    esac
+done
 
 for (( n=$1; n<=$2; n+=$3 ))
 do 
@@ -42,28 +68,91 @@ do
   ./simulation_secuencial 1 ../Starting_Configurations/bin_files/random.bin >/dev/null
   cp ../Graphics/data/1.bin Check/secuential.bin >/dev/null
 
-  #Delete old data
-  if [ -d ../Graphics/data ]; then
-    rm -f ../Graphics/data/*.csv 2>/dev/null
-    rm -f ../Graphics/data/*.bin 2>/dev/null
-  else
-    mkdir ../Graphics/data 2>/dev/null
-  fi
+  if [ $cuda == true -a $Open_MP == true ]; then
+    #Delete old data
+    if [ -d ../Graphics/data ]; then
+      rm -f ../Graphics/data/*.csv 2>/dev/null
+      rm -f ../Graphics/data/*.bin 2>/dev/null
+    else
+      mkdir ../Graphics/data 2>/dev/null
+    fi
 
-  ./simulation_cuda 1 ../Starting_Configurations/bin_files/random.bin >/dev/null
+    ./simulation_OpenMP 1 ../Starting_Configurations/bin_files/random.bin >/dev/null
 
-  cp ../Graphics/data/1.bin Check/cuda.bin >/dev/null
+    cp ../Graphics/data/1.bin Check/OpenMP.bin >/dev/null
 
-  cd Check/
-  ./compare $1 secuential.bin cuda.bin >/dev/null
+    ./simulation_cuda 1 ../Starting_Configurations/bin_files/random.bin >/dev/null
 
-  if [ $? == 1 ];
-  then
-    echo -e "${CLEAR_LINE}${GREEN} PASSED ${NC} $n bodies"
-  else
-    echo -e "${CLEAR_LINE} ${RED} FAILED ${NC} $n bodies"
-    exit
+    cp ../Graphics/data/1.bin Check/cuda.bin >/dev/null
+
+    cd Check/
+    ./compare $1 secuential.bin cuda.bin >/dev/null
+    result_cuda=$?
+    ./compare $1 secuential.bin OpenMP.bin >/dev/null
+    result_OpenMP=$?
+    if [ $result_cuda == 1 -a $result_OpenMP == 1 ];
+    then
+      echo -e "${CLEAR_LINE}${GREEN} PASSED ${NC} $n bodies"
+    elif [ $result_cuda != 1 -a $result_OpenMP == 1 ]; then
+      echo -e "${CLEAR_LINE}${RED} CUDA FAILED ${NC} $n bodies"
+      exit 0
+    elif [ $result_cuda == 1 -a $result_OpenMP != 1 ]; then
+      echo -e "${CLEAR_LINE}${RED} OPEN_MP FAILED ${NC} $n bodies"
+      exit 0
+    else
+      echo -e "${CLEAR_LINE}${RED} OPEN_MP & CUDA FAILED ${NC} $n bodies"
+      exit 0
+    fi
+  elif [ $cuda == false -a $Open_MP == true ]; then
+    #Delete old data
+    if [ -d ../Graphics/data ]; then
+      rm -f ../Graphics/data/*.csv 2>/dev/null
+      rm -f ../Graphics/data/*.bin 2>/dev/null
+    else
+      mkdir ../Graphics/data 2>/dev/null
+    fi
+
+    ./simulation_OpenMP 1 ../Starting_Configurations/bin_files/random.bin >/dev/null
+
+    cp ../Graphics/data/1.bin Check/OpenMP.bin >/dev/null
+
+    cd Check/
+    ./compare $1 secuential.bin OpenMP.bin >/dev/null
+
+    if [ $? == 1 ];
+    then
+      echo -e "${CLEAR_LINE}${GREEN} PASSED ${NC} $n bodies"
+    else
+      echo -e "${CLEAR_LINE}${RED} FAILED ${NC} $n bodies"
+      exit 0
+    fi
+  else 
+    #Delete old data
+    if [ -d ../Graphics/data ]; then
+      rm -f ../Graphics/data/*.csv 2>/dev/null
+      rm -f ../Graphics/data/*.bin 2>/dev/null
+    else
+      mkdir ../Graphics/data 2>/dev/null
+    fi
+
+    ./simulation_cuda 1 ../Starting_Configurations/bin_files/random.bin >/dev/null
+
+    cp ../Graphics/data/1.bin Check/cuda.bin >/dev/null
+
+    cd Check/
+    ./compare $1 secuential.bin cuda.bin >/dev/null
+
+    if [ $? == 1 ];
+    then
+      echo -e "${CLEAR_LINE}${GREEN} PASSED ${NC} $n bodies"
+    else
+      echo -e "${CLEAR_LINE}${RED} FAILED ${NC} $n bodies"
+      exit 0
+    fi
   fi
 done
+
+exit 1
+
 
 
