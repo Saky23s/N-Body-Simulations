03dd966131c6f005ccc051d0a1959fd72f2c5ea1
Saky23s <curtur666@gmail.com>
2024-06-23 17:41:29 +0200

    Doxygen comments

--- Solucion_Ingenua/src/main.c
+++ Solucion_Ingenua/src/main.c
@@ -1,9 +1,20 @@
+/** 
+ * @file main.c
+ * @author Santiago Salas santiago.salas@estudiante.uam.es
+ * 
+ * The main file of the secuential and OpenMP implementation, it must be a c file.
+ * Does the same as main.cpp but with c sintax.
+ * 
+ * It checks arguments, creates a simulation using the provided starting configuration file
+ * runs the simulation and frees it when its done.
+ * 
+ * @param T (double): The internal time that the simulation should last measured in seconds
+ * @param filepath (string): The filepath to the starting configuration file of the simulation, must be a csv or bin file
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "../inc/simulation.h"
-
-
 
 int main(int argc, char **argv )
 {   
--- Solucion_Ingenua/src/main.cpp
+++ Solucion_Ingenua/src/main.cpp
@@ -1,3 +1,17 @@
+/** 
+ * @file main.cpp
+ * @author Santiago Salas santiago.salas@estudiante.uam.es
+ * 
+ * The main file of the cuda implementation, it must be a cpp file.
+ * Does the same as main.c but with some of cpp sintax.
+ * 
+ * It checks arguments, creates a simulation using the provided starting configuration file
+ * runs the simulation and frees it when its done.
+ * 
+ * @param T (double): The internal time that the simulation should last measured in seconds
+ * @param filepath (string): The filepath to the starting configuration file of the simulation, must be a csv or bin file
+ */
+
 #include <iostream>
 #include <cstdlib>
 #include "../inc/simulation.h"
