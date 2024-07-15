574e3f345e2e585978998496932514c5460eab09
Saky23s <curtur666@gmail.com>
2024-07-15 23:07:48 +0200

    Join create plots scripts into only one with arguments

---
+++ Plots/create_plots.sh
@@ -0,0 +1,40 @@
+#!/bin/bash 
+
+# Check al least the two must have parameters are provided
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <Max_N> <Steps>"
+    exit 1
+fi
+
+#Generate fresh executables
+make clean *>/dev/null
+make all  >/dev/null
+
+TYPE="secuencial";
+for arg in "$@" 
+do
+    case $arg in
+        -o) 
+            
+            TYPE="OpenMP";
+            ;;
+        -c) 
+            TYPE="cuda";
+            ;;
+        *)
+            ;;
+    esac
+done
+
+#Delete old data just for safety
+if [ -d ./dev/shm/data ]; then
+  rm -f ./dev/shm/data/*.csv
+  rm -f ./dev/shm/data/*.bin
+else
+  mkdir ./dev/shm/data
+fi
+
+./time_simulations_${TYPE} $1 $2
+
+#plot results
+gnuplot> load 'times.p'
