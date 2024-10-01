285669363a904c147bfb08321b31644adfdf5860
Saky23s <curtur666@gmail.com>
2024-10-01 18:45:58 +0200

    added vectorial to plot all

--- Plots/plot_all.sh
+++ Plots/plot_all.sh
@@ -1,39 +1,43 @@
 #!/bin/bash 
 
+make clean
+make
+
 #Delete old data
-if [ -d /dev/shm/data ]; then
-rm -f /dev/shm/data/*.csv >/dev/null
-rm -f /dev/shm/data/*.bin >/dev/null
-else
-mkdir /dev/shm/data >/dev/null
-fi
+./create_shm_directory.sh
 
 ./time_simulations_secuencial 1 2001 100 
 cp times.log Secuencial.log
 cp Times.jpeg Secuencial.jpeg
 
 #Delete old data
-if [ -d /dev/shm/data ]; then
-rm -f /dev/shm/data/*.csv >/dev/null
-rm -f /dev/shm/data/*.bin >/dev/null
-else
-mkdir /dev/shm/data >/dev/null
-fi
+./create_shm_directory.sh
 
-./time_simulations_openmp 1 5001 100 
+./time_simulations_secuencial_vectorial 1 2001 100 
+cp times.log Secuencial.log
+cp Times.jpeg Secuencial.jpeg
+
+#Delete old data
+./create_shm_directory.sh
+
+./time_simulations_OpenMP 1 5001 100 
 cp times.log OpenMP.log
 cp Times.jpeg OpenMP.jpeg
 
-if [ -d /dev/shm/data ]; then
-rm -f /dev/shm/data/*.csv >/dev/null
-rm -f /dev/shm/data/*.bin >/dev/null
-else
-mkdir /dev/shm/data >/dev/null
-fi
+#Delete old data
+./create_shm_directory.sh
+
+./time_simulations_OpenMP_vectorial 1 5001 100 
+cp times.log OpenMP_vectorial.log
+cp Times.jpeg OpenMP_vectorial.jpeg
+
+./create_shm_directory.sh
 
 ./time_simulations_cuda 1 5001 100 
 cp times.log Cuda.log
 cp Times.jpeg Cuda.jpeg
+
+./create_shm_directory.sh
 
 ./time_treecode.sh 1 25001 100
 cp times.log Treecode.log
