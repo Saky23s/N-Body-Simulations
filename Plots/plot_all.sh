9a0abdb11b7b19c80f4b54b45fb78d98a878c786
Saky23s <curtur666@gmail.com>
2024-10-02 16:19:27 +0200

    Fixed mistake creating logs

--- Plots/plot_all.sh
+++ Plots/plot_all.sh
@@ -6,16 +6,16 @@ make
 #Delete old data
 ./create_shm_directory.sh
 
-./time_simulations_secuencial 1 2001 100 
+./time_simulations_secuencial 1 3001 100 
 cp times.log Secuencial.log
 cp Times.jpeg Secuencial.jpeg
 
 #Delete old data
 ./create_shm_directory.sh
 
-./time_simulations_secuencial_vectorial 1 2001 100 
-cp times.log Secuencial.log
-cp Times.jpeg Secuencial.jpeg
+./time_simulations_secuencial_vectorial 1 5001 100 
+cp times.log Secuencial_vectorial.log
+cp Times.jpeg Secuencial_vectorial.jpeg
 
 #Delete old data
 ./create_shm_directory.sh
@@ -33,7 +33,7 @@ cp Times.jpeg OpenMP_vectorial.jpeg
 
 ./create_shm_directory.sh
 
-./time_simulations_cuda 1 5001 100 
+./time_simulations_cuda 1 10001 100 
 cp times.log Cuda.log
 cp Times.jpeg Cuda.jpeg
 
