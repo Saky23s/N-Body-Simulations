17afdfe0dbd59c5e77596285f584b1998eada24e
Saky23s <curtur666@gmail.com>
2024-10-12 20:19:04 +0200

    Mp4 made with more support

--- Graphics/create_mp4.sh
+++ Graphics/create_mp4.sh
@@ -10,4 +10,4 @@ fi
 cargo run
 n= ls -1q img/*.png | wc -l
 
-ffmpeg -f image2 -framerate 25 -i img/%05d.png -vcodec libx264 -crf 22 simulation.mp4
+ffmpeg -f image2 -framerate 60 -i img/%05d.png -vf "scale=1920:1080" -vcodec libx264 -pix_fmt yuv420p -crf 28 -movflags faststart -preset medium simulation.mp4
